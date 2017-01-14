a6c47dbe871609432da1f12c427a78b3fbeb64a9
Alex Hetmanenko <mapache.voodoo@gmail.com>
2017-01-14 18:49:22 +0200

    Modified .html, .css, .js files

--- scripts/game.js
+++ scripts/game.js
@@ -7,8 +7,8 @@ function startGame() {
 		scissors: 'paper',
 		paper: 'rock'
 	}
-		document.getElementById("userChoise").innerHTML = 'You: ' + userChoise; 
-		document.getElementById("pcChoise").innerHTML = 'AI: ' + pcChoise;
+	document.getElementById("userChoise").innerHTML = 'You: ' + userChoise; 
+	document.getElementById("pcChoise").innerHTML = 'AI: ' + pcChoise;
 	
 	if (userChoise === pcChoise) {
 		document.getElementById("result").style.color = "#FFD700";
@@ -43,18 +43,18 @@ function getUserChoise() {
 	var actualChoise;
 
 	while (!actualChoise) {
-	
-	switch (imgChoise) {
-		case '1':
+		
+		switch (imgChoise) {
+			case '1':
 			actualChoise = 'rock';
 			break;
-		case '2':
+			case '2':
 			actualChoise = 'scissors';
 			break;
-		case '3':
+			case '3':
 			actualChoise = 'paper';
 			break;
-		default:
+			default:
 			document.getElementById("result").innerHTML = 'Please make your choise!';
 			break;
 		}
@@ -63,33 +63,33 @@ function getUserChoise() {
 }
 
 function winsCounter() {
-  var winsCounter = document.getElementById('winsCounter');
-  var count = Number(winsCounter.innerHTML);
+	var winsCounter = document.getElementById('winsCounter');
+	var count = Number(winsCounter.innerHTML);
 
-  winsCounter.innerHTML = count += 1;
+	winsCounter.innerHTML = count += 1;
 	return winsCounter;
 };
 
 function losesCounter() {
-  var losesCounter = document.getElementById('losesCounter');
-  var count = Number(losesCounter.innerHTML);
+	var losesCounter = document.getElementById('losesCounter');
+	var count = Number(losesCounter.innerHTML);
 
-  losesCounter.innerHTML = count += 1;
+	losesCounter.innerHTML = count += 1;
 	return losesCounter;
 };
 
 function opacity() {
 	if (imgChoise == 1) {
-	document.getElementById("1").style.opacity = '1',
-	document.getElementById("2").style.opacity = '0.66',
-	document.getElementById("3").style.opacity = '0.66';
-} else if (imgChoise == 2) {
-	document.getElementById("1").style.opacity = '0.66',
-	document.getElementById("2").style.opacity = '1',
-	document.getElementById("3").style.opacity = '0.66';
-} else {
-	document.getElementById("1").style.opacity = '0.66',
-	document.getElementById("2").style.opacity = '0.66',
-	document.getElementById("3").style.opacity = '1';
-}
+		document.getElementById("1").style.opacity = '1',
+		document.getElementById("2").style.opacity = '0.66',
+		document.getElementById("3").style.opacity = '0.66';
+	} else if (imgChoise == 2) {
+		document.getElementById("1").style.opacity = '0.66',
+		document.getElementById("2").style.opacity = '1',
+		document.getElementById("3").style.opacity = '0.66';
+	} else {
+		document.getElementById("1").style.opacity = '0.66',
+		document.getElementById("2").style.opacity = '0.66',
+		document.getElementById("3").style.opacity = '1';
+	}
 }
